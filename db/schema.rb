d5a5da4ed014bb4858a8484725e1f125dab57a3f
Baki Udemy Course <vernes.pendic@gmail.com>
2017-10-03 19:01:41 +0000

    Commit to check out active card error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003135222) do
+ActiveRecord::Schema.define(version: 20171003181457) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string   "email"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "token"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
